c3d40cbacdcb1a936aac8963aa3f26eeb9c9f7a3
Cooper <47166588+Cooper-Pilot@users.noreply.github.com>
2023-12-07 19:06:00 +0000

    d3p1 done

--- day3/Day3.cs
+++ day3/Day3.cs
@@ -6,26 +6,51 @@ public class Day3 : AocDay
     public override void Run(int part)
 	{
         var lines = File.ReadAllLines("day3/input");
-        StringBuilder id = new();
-        List<Part> parts = new();
+        int length = 0;
+        List<Location> locations = new();
         for (int y = 0; y < lines.Length; y++)
         {
             var line = lines[y];
-            for (int x = 0; x < line.Length; x++)
+            for (int x = 0;; x++)
             {
-                var c = line[x];
-                if (!char.IsDigit(c))
+                if (x < line.Length && char.IsDigit(line[x]))
                 {
-                    if (id.Length > 0)
-                    {
-                        parts.Add(new(int.Parse(id.ToString()), x - id.Length, y));
-                        id.Clear();
-                    }
+                    length++;
                     continue;
                 }
-                id.Append(c);
+                if (length > 0)
+                    locations.Add(new(length, x - length, y));
+                length = 0;
+                if (x >= line.Length)
+                    break;
             }
         }
+        int sum = 0;
+        foreach(var loc in locations)
+        {
+            for (int y = loc.y - 1; y <= loc.y + 1; y++)
+            {
+                if (y < 0 || y >= lines.Length)
+                    continue;
+                var line = lines[y];
+                for (int x = loc.x - 1; x < loc.x + loc.length + 1; x++)
+                {
+                    if (x < 0 || x >= line.Length)
+                        continue;
+                    var c = line[x];
+                    if (!char.IsDigit(c) && c != '.')
+                    {
+                        var locLine = lines[loc.y];
+                        var str = locLine[loc.x .. (loc.x + loc.length)];
+                        var num = int.Parse(str);
+                        sum += num;
+                        goto NextLocation;
+                    }
+                }
+                NextLocation:;
+            }
+        }
+        Console.WriteLine(sum);
     }
 }
-public record Part(int id, int x, int y);
+public record Location(int length, int x, int y);

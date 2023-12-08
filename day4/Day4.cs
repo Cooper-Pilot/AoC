4ea815c60eb38a84353fd38f9c62ea14ccaa25a7
Cooper <47166588+Cooper-Pilot@users.noreply.github.com>
2023-12-08 19:55:41 +0000

    d4p2 done

--- day4/Day4.cs
+++ day4/Day4.cs
@@ -2,12 +2,12 @@ using System.Text;
 namespace AoC.Day4;
 public class Day4 : AocDay
 {
+    List<int> cardValues = new();
     public override int GetDay() => 4;
     public override void Run(int part)
 	{
         var lines = File.ReadAllLines("day4/input");
         int sum = 0;
-        List<int> timesToCopy = new();
         foreach(var line in lines)
         {
             var data = line.Split(':')[1].Split('|');
@@ -16,18 +16,26 @@ public class Day4 : AocDay
             var matchCount = cardNums.Where(i => winningNums.Contains(i)).Count();
             if (part == 1 && matchCount > 0)
                 sum += (int)Math.Pow(2, matchCount - 1);
-            if (part != 2)
-                continue;
-            sum += 1 + timesToCopy.Count;
-            for(int i = timesToCopy.Count - 1; i >= 0; i--)
+            if (part == 2)
+                cardValues.Add(matchCount);
+        }
+        if (part == 2)
+        {
+            for(int i = 0; i < cardValues.Count; i++)
             {
-                timesToCopy[i]--;
-                if (timesToCopy[i] == 0)
-                    timesToCopy.RemoveAt(i);
+                sum += GetTotalValue(i);
             }
-            if (matchCount > 0)
-                timesToCopy.Add(matchCount);
         }
         Console.WriteLine(sum);
+    }
+    public int GetTotalValue(int cardNum)
+    {
+        int sum = 1;
+        int lastCard = cardNum + cardValues[cardNum];
+        for (int i = cardNum + 1; i <= lastCard; i++)
+        {
+            sum += GetTotalValue(i);
+        }
+        return sum;
     }
 }

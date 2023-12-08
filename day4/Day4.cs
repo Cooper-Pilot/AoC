8ff30b38349520ff75a623d3ffce05811debee3c
Cooper <47166588+Cooper-Pilot@users.noreply.github.com>
2023-12-08 19:34:11 +0000

    failed attempted at part 2

--- day4/Day4.cs
+++ day4/Day4.cs
@@ -7,14 +7,26 @@ public class Day4 : AocDay
 	{
         var lines = File.ReadAllLines("day4/input");
         int sum = 0;
+        List<int> timesToCopy = new();
         foreach(var line in lines)
         {
             var data = line.Split(':')[1].Split('|');
             var cardNums = data[0].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(s => int.Parse(s));
             var winningNums = data[1].Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(s => int.Parse(s));
             var matchCount = cardNums.Where(i => winningNums.Contains(i)).Count();
-            if (matchCount > 0)
+            if (part == 1 && matchCount > 0)
                 sum += (int)Math.Pow(2, matchCount - 1);
+            if (part != 2)
+                continue;
+            sum += 1 + timesToCopy.Count;
+            for(int i = timesToCopy.Count - 1; i >= 0; i--)
+            {
+                timesToCopy[i]--;
+                if (timesToCopy[i] == 0)
+                    timesToCopy.RemoveAt(i);
+            }
+            if (matchCount > 0)
+                timesToCopy.Add(matchCount);
         }
         Console.WriteLine(sum);
     }

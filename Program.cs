6eb00ccbb0ada2daef7c69564b815258ed121c26
Cooper <47166588+Cooper-Pilot@users.noreply.github.com>
2023-12-09 13:53:49 +0000

    err checking for getday

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
-﻿using System.Linq;
-using System.Runtime.InteropServices;
-Dictionary<int, AocDay> aocDays = [];
+﻿Dictionary<int, AocDay> aocDays = [];
 var types =
-from assembly in AppDomain.CurrentDomain.GetAssemblies()
+    from assembly in AppDomain.CurrentDomain.GetAssemblies()
     from type in assembly.GetTypes()
     where type.IsSubclassOf(typeof(AocDay))
     select type;
-foreach(var type in types)
+foreach (var type in types)
 {
-    if (Activator.CreateInstance(type) is AocDay aocDay)
+    AocDay aocDay = (AocDay)Activator.CreateInstance(type)!;
+    try
     {
         var _day = aocDay.GetDay();
         if (aocDays.TryGetValue(_day, out var foundDay))
@@ -18,22 +17,26 @@ foreach(var type in types)
         }
         aocDays.Add(_day, aocDay);
     }
+    catch (NotImplementedException)
+    {
+        Console.Error.WriteLine($"\"{type.Name}\" has not implemented \"{nameof(aocDay.GetDay)}\", skipping...");
+    }
 }
 int day = 1, part = 1;
 try
 {
-if (args.Length > 2)
-    throw new ArgumentException("Too many args, expected 2 or fewer");
-if (args.Length > 0 && !int.TryParse(args[0], out day))
-    throw new ArgumentException($"First arg \"{args[0]}\" must be an integer");
-if (args.Length == 2 && !int.TryParse(args[1], out part))
-    throw new ArgumentException($"Second arg \"{args[1]}\" must be an integer");
-if (!aocDays.ContainsKey(day))
-    throw new ArgumentException($"First arg \"{day}\" err: day does not exist");
-if (part < 1 || part > 2)
-    throw new ArgumentException($"Second arg \"{part}\" must be 1 or 2");
-Console.WriteLine($"Starting day {day} part {part}");
-aocDays[day].Run(part);
+    if (args.Length > 2)
+        throw new ArgumentException("Too many args, expected 2 or fewer");
+    if (args.Length > 0 && !int.TryParse(args[0], out day))
+        throw new ArgumentException($"First arg \"{args[0]}\" must be an integer");
+    if (args.Length == 2 && !int.TryParse(args[1], out part))
+        throw new ArgumentException($"Second arg \"{args[1]}\" must be an integer");
+    if (!aocDays.ContainsKey(day))
+        throw new ArgumentException($"First arg \"{day}\" err: day does not exist");
+    if (part < 1 || part > 2)
+        throw new ArgumentException($"Second arg \"{part}\" must be 1 or 2");
+    Console.WriteLine($"Starting day {day} part {part}");
+    aocDays[day].Run(part);
 }
 catch (ArgumentException e)
 {
---
+++ day5/Day5.cs
@@ -0,0 +1,6 @@
+using System.Text;
+namespace AoC.Day4;
+public class Day5 : AocDay
+{
+    
+}
